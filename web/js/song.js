41ed41694129bcf60d0dd41b76a6fb983acb453e
Kyle Mattimore <kmattimo@umich.edu>
2015-10-06 14:00:32 -0400

    knockout observables

--- web/js/song.js
+++ web/js/song.js
@@ -8,18 +8,24 @@ var loaderWrapper = function (URLs) {
 	this.loader.onload = function () { 	song = new Song(this); };
 	this.playing = false;
 	this.loader.send();
-}
+};
 
 var Song = function(audioLoader) {
 	console.log("creating song");
+	var self = this;
 	this.loaded = ko.observable(false);
 	this.playing = ko.observable(false);
-	
+
 	this.buffers = audioLoader.response;
 	this.loader = audioLoader;
 	this.sources = [];
 	this.gainNodes = [];
-//	this.ctx = audioLoader.ctx; 
+	this.seekTime = ko.observable();
+	this.seekFraction = ko.computed(function() {
+		//return (this.seekTime() / this.duration() );
+		console.log(self.seekTime());
+		return (self.seekTime() );
+	});
 	
 	$("#scrub").attr("max",this.buffers[0].duration);
 	ko.applyBindings(this);
@@ -36,7 +42,11 @@ var Song = function(audioLoader) {
 			this.gainNodes[i].connect(this.loader.ctx.destination);
 		}
 	console.log("initialized buffers");
-	}
+	};
+	
+	setInterval(function() {   
+		self.seekTime(self.loader.ctx.currentTime);
+	}, 1000);
 		
 	this.play = function (time) {
 		if(this.playing() == true) {
@@ -49,7 +59,7 @@ var Song = function(audioLoader) {
 		this.sources.forEach(function(thisSource) { thisSource.start(0, time)} ) ;
 		//this.playing = true;
 		this.playing(true);
-	}
+	};
 	
 	this.stop = function () {
 		if(this.playing() == true)
@@ -57,7 +67,7 @@ var Song = function(audioLoader) {
 			this.sources.forEach(function(thisSource) { thisSource.stop() } ) ;
 			this.playing(false);
 			}
-	}
+	};
 	
 	this.changeTrackVolume = function(volume, i) {
 		console.log("track volume" , volume, i);
@@ -66,8 +76,16 @@ var Song = function(audioLoader) {
 		var fraction = parseInt(volume) / 100;
 		
 		this.gainNodes[i].gain.value = (fraction * fraction) ;
-	}	
-}
+	};
+	
+	this.duration = function() {
+			return this.buffers[0].duration;
+	};
+	// this.seekTime = function() {
+	// 		return this.loader.ctx.currentTime;
+	// };
+
+};
 
 $(document).ready(function() {
 	var audioLoader = new loaderWrapper(window.URLs);
