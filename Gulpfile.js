eada4a4657ddf32699202f8435b75399068726be
Kyle Mattimore <kmattimo@umich.edu>
2015-11-03 23:45:22 -0500

    YEAH BROWSERSYNC
    Former-commit-id: df7263a4155e4a555d5040243d8b9d311dd3a3ee
    Former-commit-id: fb0f1941a6621e295bab744e7096a40e945a6c46

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,41 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 //all scss imported in site.scss, except page-specific stuff
 var input = './web/scss/site.scss';
 var watchFiles = './web/scss/**/*.scss';
+var templateFiles = './app/Resources/views/**/*.twig';
+var jsFiles= './web/js/**/*.js';
 var output = './web/css/';
 
 gulp.task('sass', function() {
     gulp.src(input)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(output))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
     gulp.watch(watchFiles,['sass']);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./web/"
+        }
+    });
+});
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        proxy: "localhost:8000"
+    });
+
+    gulp.watch(watchFiles, ['sass']);
+    gulp.watch([templateFiles, jsFiles]).on('change', browserSync.reload);
 });
 
 //Watch task
